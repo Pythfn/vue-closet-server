56d6c2edb791a54fa06051a253af2dd055ee92c4
Pythfn <rusonosy@outlook.com>
2019-02-01 12:14:12 +0800

    注册接口

--- config/db.js
+++
@@ -1,7 +0,0 @@
-export const sqlserver = {
-  host: 'localhost',
-  user: 'root',
-  pw: '123456',
-  port: 3306,
-  database: 'vct'
-}
--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,8 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
-router.post('/register', async function (ctx, next) {
-  const sss = getuserid()
-  console.log(sss)
+//  注册接口
+router.post('/signup', async function (ctx, next) {
   const {username, pass, email, phone, sex} = await ctx.request.body
   if (!username || !pass || !email) {
     ctx.body = {
@@ -18,24 +17,38 @@ router.post('/register', async function (ctx, next) {
       msg: '注册信息有误'
     }
   } else {
-    const beforereg = 'SELECT username FROM users where username = (?)'
-    const bfr = db.query(beforereg, username)
-    await bfr.then((res) => {
+    const sqlbeforereg = 'SELECT username FROM users where username = (?)'
+    let bfr = db.query(sqlbeforereg, username)
+    await bfr.then(async res => {
       if (res.length) {
         ctx.body = {
           code: -1,
           msg:'用户名已存在'
         }
       } else {
-        ctx.body = {
-          msg: sss
-        }
+        //  数据库无重复用户名则进行写库
+        const userid = getuserid()
+        const sqlReg = 'INSERT INTO users(userid, username, password, email) VALUES(?,?,?,?)'
+        const sqlRegValues = [userid, username, pass, email]
+        let reg = db.query(sqlReg, sqlRegValues)
+        await reg.then(res => {
+          ctx.body = {
+            code: 1,
+            msg: '注册成功'
+          }
+        }).catch((e) => {
+          ctx.body = {
+            code: -1,
+            msg: e
+          }
+        })
       }
     })
   }
 })
 
-router.get('/login', async (ctx, next) => {
+//  登陆接口
+router.get('/signin', async (ctx, next) => {
   const sql = 'select * from users where username = (?) and password = (?)'
   const values = ['abcde', 123456]
   const d = db.query(sql, values)
@@ -44,18 +57,6 @@ router.get('/login', async (ctx, next) => {
   })
   const req = ctx.request.body
   ctx.body = await d
-})
-
-router.get('/getcolorlist', async (ctx, next) => {
-  ctx.body = {
-    code: 0,
-    data:{
-      colorlist: {
-        '黑色': ['黑色','淡黑色',],
-        '白色': ['纯白色','乳白色','米白色']
-      }
-    }
-  }
 })
 
 module.exports = router
