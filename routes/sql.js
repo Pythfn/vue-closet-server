e16fcf5bf8e641c036a390f8eaa62407cb780c5e
Pythfn <rusonosy@outlook.com>
2019-02-09 01:28:54 +0800

    增加删除商品接口

--- routes/sql.js
+++ routes/sql.js
@@ -9,8 +9,7 @@ router.prefix('/api')
 router.post('/additem', async (ctx, next) => {
   //  校验token，取到userid
   const token = ctx.header.authorization.split(' ')[1]
-  const verify = jwtverify(token, sc.jwtsecret)
-  await verify.then(async (decoded) => {
+  await jwtverify(token, sc.jwtsecret).then(async (decoded) => {
     const userid = decoded.userid
     let { name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark } = ctx.request.body
     //  转短链接
@@ -45,7 +44,8 @@ router.post('/additem', async (ctx, next) => {
   })
 })
 
-router.post('/updateItem', async (ctx, next) => {
+//  商品修改接口
+router.post('/updateitem', async (ctx, next) => {
   //  校验token，取到userid
   const token = ctx.header.authorization.split(' ')[1]
   const verify = jwtverify(token, sc.jwtsecret)
@@ -55,8 +55,9 @@ router.post('/updateItem', async (ctx, next) => {
     picurl = await toShortUrl(picurl)
     if (userid) {
       try {
-        let sqlUpdate = "UPDATE vcitems SET name = ?, price = ?, cost = ?, type = ?, size = ?, rate = ?, color = ?, colorcode = ?, tags = ?, pic = ?, picurl = ?, itemlink = ?, remark = ? WHERE itemid = ?"
-        let sqlUpdateValues = [name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark, itemid]
+        //  修改语句中加入判断userid的条件，防止他人篡改
+        let sqlUpdate = "UPDATE vcitems SET name = ?, price = ?, cost = ?, type = ?, size = ?, rate = ?, color = ?, colorcode = ?, tags = ?, pic = ?, picurl = ?, itemlink = ?, remark = ? WHERE itemid = ? AND userid = ?"
+        let sqlUpdateValues = [name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark, itemid, userid]
         await db.query(sqlUpdate, sqlUpdateValues).then((res) => {
           ctx.body = {
             code: 1,
@@ -81,6 +82,39 @@ router.post('/updateItem', async (ctx, next) => {
   })
 })
 
+//  商品标记删除接口
+router.post('/removeitem', async (ctx, next) => {
+  const token = ctx.header.authorization.split(' ')[1]
+  await jwtverify(token, sc.jwtsecret).then(async decoded => {
+    const userid = decoded.userid
+    if (userid) {
+      try {
+        const { itemid } = ctx.request.body
+        let sqlRemove = 'UPDATE vcitems SET isdelete = ? WHERE itemid = ? AND userid = ?'
+        let sqlRemoveValues = [1, itemid, userid]
+        await db.query(sqlRemove, sqlRemoveValues).then((res) => {
+          ctx.body = {
+            code: 1,
+            msg: '删除成功'
+          }
+        })
+      } catch (err) {
+        console.log('###SQLADDERROR### ' + err)
+        ctx.body = {
+          code: -1,
+          msg: '删除失败'
+        }
+      }
+    }
+  }).catch(err => {
+    console.log('###JWTERROR### ' + err)
+    ctx.body = {
+      code: -1,
+      msg: '请重新登陆'
+    }
+  })
+})
+
 //  获取商品接口
 router.post('/getitems', async (ctx, next) => {
   //  校验token，取到userid
@@ -89,7 +123,7 @@ router.post('/getitems', async (ctx, next) => {
     const userid = decoded.userid
     if (userid) {
       try {
-        const sqlGet = 'SELECT * FROM vcitems WHERE userid = ?'
+        const sqlGet = 'SELECT * FROM vcitems WHERE userid = ? AND isdelete <> 1'
         await db.query(sqlGet, userid).then((itemlist) => {
           ctx.body = {
             code: 1,
