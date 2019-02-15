a6ceb95b1d9c2b8d6c0ce0a322df63456ee8b4c9
Pythfn <rusonosy@outlook.com>
2019-02-16 02:56:37 +0800

    更新添加修改修改 表结构改动

--- routes/sql.js
+++ routes/sql.js
@@ -3,6 +3,7 @@ const jwtverify = require('./utils/jwtverify')
 const sc = require('../config')
 const db = require('../lib/dbquery.js')
 const toShortUrl = require('./utils/toShortUrl')
+const getFormatDate = require('./utils/getFormatDate')
 router.prefix('/api')
 
 //  添加商品接口
@@ -11,14 +12,15 @@ router.post('/additem', async (ctx, next) => {
   const token = ctx.header.authorization.split(' ')[1]
   await jwtverify(token, sc.jwtsecret).then(async (decoded) => {
     const userid = decoded.userid
-    let { name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark } = ctx.request.body
+    let { name, price, cost, type, size, rate, color, colorcode, tags, cover, pic, picurl, itemlink, remark, isstar, ispublic } = ctx.request.body
     //  转短链接
     picurl = await toShortUrl(picurl)
 
     if (userid) {
       try {
-        const sqlAdd = 'INSERT INTO vcitems(name, price, cost, type, size, rate, color, colorcode, tags,  pic, picurl, itemlink, remark, isdelete, userinfo, userid) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
-        const sqlAddValues = [name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark, 0, null, userid]
+        let createtime = getFormatDate()
+        const sqlAdd = 'INSERT INTO vcitems(name, price, cost, type, size, rate, color, colorcode, tags, cover, pic, picurl, itemlink, remark, isdelete, userinfo, userid, isstar, ispublic, createtime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        const sqlAddValues = [name, price, cost, type, size, rate, color, colorcode, tags, cover, pic, picurl, itemlink, remark, 0, null, userid, isstar, ispublic, createtime]
         const dbAdd = db.query(sqlAdd, sqlAddValues)
         await dbAdd.then((res) => {
           ctx.body = {
@@ -51,13 +53,14 @@ router.post('/updateitem', async (ctx, next) => {
   const verify = jwtverify(token, sc.jwtsecret)
   await verify.then(async (decoded) => {
     const userid = decoded.userid
-    let { itemid, name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark } = ctx.request.body
+    let { itemid, name, price, cost, type, size, rate, color, colorcode, tags, cover, pic, picurl, itemlink, remark, isstar, ispublic } = ctx.request.body
     picurl = await toShortUrl(picurl)
     if (userid) {
       try {
         //  修改语句中加入判断userid的条件，防止他人篡改
-        let sqlUpdate = "UPDATE vcitems SET name = ?, price = ?, cost = ?, type = ?, size = ?, rate = ?, color = ?, colorcode = ?, tags = ?, pic = ?, picurl = ?, itemlink = ?, remark = ? WHERE itemid = ? AND userid = ?"
-        let sqlUpdateValues = [name, price, cost, type, size, rate, color, colorcode, tags, pic, picurl, itemlink, remark, itemid, userid]
+        let updatetime = getFormatDate()
+        let sqlUpdate = "UPDATE vcitems SET name = ?, price = ?, cost = ?, type = ?, size = ?, rate = ?, color = ?, colorcode = ?, tags = ?, cover = ?, pic = ?, picurl = ?, itemlink = ?, remark = ?, isstar = ?, ispublic = ?, updatetime = ? WHERE itemid = ? AND userid = ?"
+        let sqlUpdateValues = [name, price, cost, type, size, rate, color, colorcode, tags, cover, pic, picurl, itemlink, remark, isstar, ispublic, updatetime, itemid, userid]
         await db.query(sqlUpdate, sqlUpdateValues).then((res) => {
           ctx.body = {
             code: 1,
---
+++ routes/utils/getFormatDate.js
@@ -0,0 +1,16 @@
+ getFormatDate = function() {
+  let date = new Date()
+  let month = date.getMonth() + 1
+  let strDate = date.getDate()
+  if (month >= 1 && month <= 9) {
+    month = "0" + month
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = "0" + strDate
+  }
+  let currentDate = date.getFullYear() + "-" + month + "-" + strDate +
+    " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+  return currentDate
+ }
+
+ module.exports = getFormatDate
